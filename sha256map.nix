1d2c025830dc4f045b7fc2883dd15b7e45aa9b34
unknown <unknown@unknown>
2025-07-14 22:07:02 +0200

    initial

---
+++ sha256map.nix
@@ -0,0 +1,17 @@
+{
+  "https://github.com/simplex-chat/simplexmq.git"."7aae6f3cbe4aa2942371f8dd968eb439ccec3b15" = "0qx7l1jq0ll1448s077v37s0rv7vlbrdbdgf46x734cl9g0gndk3";
+  "https://github.com/simplex-chat/hs-socks.git"."a30cc7a79a08d8108316094f8f2f82a0c5e1ac51" = "0yasvnr7g91k76mjkamvzab2kvlb1g5pspjyjn2fr6v83swjhj38";
+  "https://github.com/kazu-yamamoto/http2.git"."f5525b755ff2418e6e6ecc69e877363b0d0bcaeb" = "0fyx0047gvhm99ilp212mmz37j84cwrfnpmssib5dw363fyb88b6";
+  "https://github.com/simplex-chat/direct-sqlcipher.git"."f814ee68b16a9447fbb467ccc8f29bdd3546bfd9" = "1ql13f4kfwkbaq7nygkxgw84213i0zm7c1a8hwvramayxl38dq5d";
+  "https://github.com/simplex-chat/sqlcipher-simple.git"."a46bd361a19376c5211f1058908fc0ae6bf42446" = "1z0r78d8f0812kxbgsm735qf6xx8lvaz27k1a0b4a2m0sshpd5gl";
+  "https://github.com/simplex-chat/aeson.git"."aab7b5a14d6c5ea64c64dcaee418de1bb00dcc2b" = "0jz7kda8gai893vyvj96fy962ncv8dcsx71fbddyy8zrvc88jfrr";
+  "https://github.com/simplex-chat/haskell-terminal.git"."f708b00009b54890172068f168bf98508ffcd495" = "0zmq7lmfsk8m340g47g5963yba7i88n4afa6z93sg9px5jv1mijj";
+  "https://github.com/simplex-chat/android-support.git"."9aa09f148089d6752ce563b14c2df1895718d806" = "0pbf2pf13v2kjzi397nr13f1h3jv0imvsq8rpiyy2qyx5vd50pqn";
+  "https://github.com/simplex-chat/network-transport.git"."0013798272a683e35ca38d2fdaf480942311fba8" = "0dnn62apgvc248df0m8ib7phrzn63wm0xs71xvlypv52j6cgwzkb";
+  "git://localhost/inline-java"."df52787444a8673a06a79d98f4ed8546a85aa295" = "sha256-svpIrxVEI9/GYaLIKylxkWqQwt4IKzFS9s0RYrN5WOo=";
+  "https://github.com/tweag/distributed-closure.git"."0eaace06ad1e9d80d13287b4e3b1e03f314082ed" = "sha256-WHZ3rTnEadTInQCOPUP67d1+ApGWQIvExwpi98R4DFM=";
+  "git://localhost/concurrent-loops"."1c5a6073039d0cfafa362411e58151c1f30da70d" = "sha256-+9p+SWrAc+EZv6MazuNETuYQoLuuDzc/EVS4C4AdIvg=";
+  "git://localhost/hcobs"."d019ab3137c8b23b2bef5eff81c382ee575e636f" = "sha256-OxgkYAiojnhq2LF27OV1TVLVhYOJOSyFajeq1Fru/nM=";
+  "git://localhost/idiot"."3e6b42cbfb8d1d0dce37b06fb92d53a1facc1819" = "sha256-gLxHgE4JBuYREtD6qQxtNhAPdbdl6kAURbkGZKedWq4==";
+  "git://localhost/idiot-cryptonite"."a6324fae28244a538cd7ee5316e30d2d82101d33" = "sha256-AdILhO1EcN204XtReJJZS77TzoJx6TsWqZtC3dCfFdU=";
+}
